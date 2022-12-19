f6d1b4e2672c5a2cbf84f58df6776ff6394cc3c6
Natalia Popova <sverdlova3@gmail.com>
2022-12-19 13:53:19 +0500

    edit argument in show_one_movie

--- movie_app/views.py
+++ movie_app/views.py
@@ -7,6 +7,6 @@ def show_all_movie(request):
     movies = Movie.objects.all()
     return render(request, 'movie_app/all_movies.html', {'movies': movies})
 
-def show_one_movie(request, id_movie:int):
-    movie = get_object_or_404(Movie, id=id_movie)
+def show_one_movie(request, slug_movie:str):
+    movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {'movie': movie})
