c379d632536583bf98c21547e3a79fc29a4e6ab0
Natalia Popova <sverdlova3@gmail.com>
2022-12-20 16:00:10 +0500

    calculated fields

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
-from django.db.models import Sum, Max, Min, Count, Avg
+from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 
 # Create your views here.
 
 def show_all_movie(request):
-    movies = Movie.objects.order_by('-rating')
+    #movies = Movie.objects.order_by('-rating')
+    movies = Movie.objects.annotate(
+        true_bool=Value(True),
+        false_bool=Value(False),
+        str_field=Value('hello'),
+        new_budget=F('budget')+100,
+    )
     agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('name'))
     return render(request, 'movie_app/all_movies.html',
                   {'movies': movies,
