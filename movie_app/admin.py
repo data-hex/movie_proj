287ade059a4926428cb3564fa5e9a85747b41f49
Natalia Popova <sverdlova3@gmail.com>
2022-12-21 15:36:50 +0500

    add class MovieAdmin and register

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Movie
 
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget']
+    list_editable = ['year']
+    ordering = ['rating']
+    list_per_page = 5
+
+admin.site.register(Movie, MovieAdmin)
