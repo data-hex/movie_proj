a5be03490f7899c47e45b13dbd4503e06381fb29
Natalia Popova <sverdlova3@gmail.com>
2022-12-22 14:55:33 +0500

    calculated field in admin

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -3,9 +3,19 @@ from .models import Movie
 
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget']
-    list_editable = ['year']
+    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
+    list_editable = ['year', 'rating']
     ordering = ['rating']
-    list_per_page = 5
+    list_per_page = 6
+
+    @admin.display(ordering='rating', description='Статус')
+    def rating_status(self, movie: Movie):
+        if movie.rating < 50:
+            return "Зачем это смотреть?!"
+        elif movie.rating < 70:
+            return "Разок можно глянуть"
+        elif movie.rating <= 85:
+            return "Зачет"
+        return 'Топчик'
 
 admin.site.register(Movie, MovieAdmin)
