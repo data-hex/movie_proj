b77442adba8d61fac21177ba80e9b31d66a3203b
Natalia Popova <sverdlova3@gmail.com>
2022-12-23 15:14:41 +0500

    create field choices

--- movie_app/models.py
+++ movie_app/models.py
@@ -5,10 +5,21 @@ from django.utils.text import slugify
 # Create your models here.
 
 class Movie(models.Model):
+    EURO = 'EUR'
+    USD = 'USD'
+    RUB = 'RUB'
+
+    СURRENCY_CHOICES = [
+        (EURO, 'Euro'),
+        (USD, 'Dollars'),
+        (RUB, 'Rubles')
+    ]
+
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True, blank=True)
     budget = models.IntegerField(default=1000000)
+    currency = models.CharField(max_length=3, choices=СURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default="", null=False)
 
     def save(self, *args, **kwargs):
