d561d8d5f85b9982897cd426e0adc9418cff5e89
Natalia Popova <sverdlova3@gmail.com>
2022-12-28 14:59:02 +0500

    new class Director

--- movie_app/models.py
+++ movie_app/models.py
@@ -5,6 +5,14 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    director_email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 class Movie(models.Model):
     EURO = 'EUR'
     USD = 'USD'
