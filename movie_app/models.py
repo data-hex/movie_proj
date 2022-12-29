7ca63aad0b7ad589c23c2374e7ec34e55bd76168
Natalia Popova <sverdlova3@gmail.com>
2022-12-29 14:57:12 +0500

    link table Movie and Director

--- movie_app/models.py
+++ movie_app/models.py
@@ -30,6 +30,7 @@ class Movie(models.Model):
     budget = models.IntegerField(default=1000000, validators=[MinValueValidator(1)])
     currency = models.CharField(max_length=3, choices=СURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default="", null=False)
+    director = models.ForeignKey(Director, on_delete=models.PROTECT, null=True)
 
     '''def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
