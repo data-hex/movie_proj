3cd75e4f7bbcebc5f9315f2dc8fe7e8d9623efa9
Natalia Popova <sverdlova3@gmail.com>
2022-12-30 10:35:54 +0500

    add func show_all_directors and show_one_director

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Movie
+from .models import Movie, Director
 from django.db.models import F, Sum, Max, Min, Count, Avg, Value
+
 
 # Create your views here.
 
 def show_all_movie(request):
-    #movies = Movie.objects.order_by('-rating')
+    # movies = Movie.objects.order_by('-rating')
     movies = Movie.objects.annotate(
         true_bool=Value(True),
         false_bool=Value(False),
         str_field=Value('hello'),
-        new_budget=F('budget')+100,
+        new_budget=F('budget') + 100,
     )
     agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('name'))
     return render(request, 'movie_app/all_movies.html',
@@ -18,6 +19,17 @@ def show_all_movie(request):
                    'agg': agg
                    })
 
-def show_one_movie(request, slug_movie:str):
+
+def show_one_movie(request, slug_movie: str):
     movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {'movie': movie})
+
+
+def show_all_directors(request):
+    directors = Director.objects.all
+    return render(request, 'movie_app/all_directors.html', {'directors': directors})
+
+
+def show_one_director(request, id_director: int):
+    director = get_object_or_404(Director, id=id_director)
+    return render(request, 'movie_app/one_director.html', {'director': director})
