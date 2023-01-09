9b8af91757312967ac4175fb881b44f2968a39d4
Natalia Popova <sverdlova3@gmail.com>
2023-01-09 14:10:09 +0500

    add path actors

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.show_all_movie),
     path('movie/<slug:slug_movie>', views.show_one_movie, name='movie-detail'),
     path('directors', views.show_all_directors),
-    path('directors/<int:id_director>', views.show_one_director, name='director-detail')
+    path('directors/<int:id_director>', views.show_one_director, name='director-detail'),
+    path('actors', views.show_all_actors),
+    path('actors/<int:id_actor>', views.show_one_actor, name='actor-detail')
+
 ]
