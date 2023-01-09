dfd3e3ecc364286b08c00a0a49cbd4df1c7545c6
Natalia Popova <sverdlova3@gmail.com>
2023-01-09 14:10:36 +0500

    add show_all_actors, show_one_actor

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Movie, Director
+from .models import Movie, Director, Actor
 from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 
 
@@ -33,3 +33,13 @@ def show_all_directors(request):
 def show_one_director(request, id_director: int):
     director = get_object_or_404(Director, id=id_director)
     return render(request, 'movie_app/one_director.html', {'director': director})
+
+
+def show_all_actors(request):
+    actors = Actor.objects.all
+    return render(request, 'movie_app/all_actors.html', {'actors': actors})
+
+
+def show_one_actor(request, id_actor: int):
+    actor = get_object_or_404(Actor, id=id_actor)
+    return render(request, 'movie_app/one_actor.html', {'actor': actor})
