734ce12059f7605b58e93ab813300623b1843a3b
Natalia Popova <sverdlova3@gmail.com>
2023-01-30 14:41:33 +0500

    ShowAllDirectors, ShowAllActors

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -1,12 +1,13 @@
 from . import views
 from django.urls import path
+from .views import ShowAllDirectors, ShowAllActors
 
 urlpatterns = [
     path('', views.show_all_movie),
     path('movie/<slug:slug_movie>', views.show_one_movie, name='movie-detail'),
-    path('directors', views.show_all_directors),
+    path('directors', ShowAllDirectors.as_view()),
     path('directors/<int:id_director>', views.show_one_director, name='director-detail'),
-    path('actors', views.show_all_actors),
+    path('actors', ShowAllActors.as_view()),
     path('actors/<int:id_actor>', views.show_one_actor, name='actor-detail')
 
 ]
