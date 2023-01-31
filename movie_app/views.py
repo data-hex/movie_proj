bb442919298a5e60fb0b88bcf66de7a5ca4a4bf1
Natalia Popova <sverdlova3@gmail.com>
2023-01-31 15:06:43 +0500

    classes ShowOneDirector, ShowOneActor

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie, Director, Actor
 from django.db.models import F, Sum, Max, Min, Count, Avg, Value
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
@@ -39,15 +39,25 @@ class ShowAllDirectors(ListView):
         queryset = super().get_queryset()
         return queryset
 
+class ShowOneDirector(DetailView):
+    template_name = 'movie_app/one_director.html'
+    model = Director
+    context_object_name = 'director'
 
 
+class ShowOneActor(DetailView):
+    template_name = 'movie_app/one_actor.html'
+    model = Actor
+    context_object_name = 'actor'
 
+
+'''
 
 def show_one_director(request, id_director: int):
     director = get_object_or_404(Director, id=id_director)
     return render(request, 'movie_app/one_director.html', {'director': director})
 
-'''
+
 def show_all_actors(request):
     actors = Actor.objects.all
     return render(request, 'movie_app/all_actors.html', {'actors': actors})'''
@@ -61,7 +71,7 @@ class ShowAllActors(ListView):
         queryset = super().get_queryset()
         return queryset
 
-
+'''
 def show_one_actor(request, id_actor: int):
     actor = get_object_or_404(Actor, id=id_actor)
-    return render(request, 'movie_app/one_actor.html', {'actor': actor})
+    return render(request, 'movie_app/one_actor.html', {'actor': actor})'''
